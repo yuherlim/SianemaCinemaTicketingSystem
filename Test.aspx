fdf1130abcebc7e4f536cb36528f7e970bebaaf2
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-03-07 14:58:51 +0800

    Test commit

---
+++ Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="SianemaCinemaTicketingSystem.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtTest" runat="server" AutoPostBack="True" OnTextChanged="txtTest_TextChanged"></asp:TextBox>
+        </div>
+        <asp:Label ID="lblTest" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Test.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SianemaCinemaTicketingSystem
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //When page first loads
+            {
+                lblTest.Text = "Welcome to test site.";
+            }
+        }
+
+        protected void txtTest_TextChanged(object sender, EventArgs e)
+        {
+            lblTest.Text = txtTest.Text.Trim();
+        }
+    }
+}
