72c34f05591d9b6daa5172f6be1a8a2b1e73b064
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-03-30 23:45:45 +0800

    js code for Register

---
+++ js/register.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    //for password field
+    show_hide_password("password-field");
+    //for confirm password field
+    show_hide_password("confirm-password-field");
+});
+
+function show_hide_password(password_field_type) {
+    $(`.${password_field_type} a`).on('click', function (event) {
+        event.preventDefault();
+        if ($(`.${password_field_type} input`).attr("type") == "text") {
+            $(`.${password_field_type} input`).attr('type', 'password');
+            $(`.${password_field_type} i.show-hide-password`).addClass("fa-eye-slash");
+            $(`.${password_field_type} i.show-hide-password`).removeClass("fa-eye");
+        } else if ($(`.${password_field_type} input`).attr("type") == "password") {
+            $(`.${password_field_type} input`).attr('type', 'text');
+            $(`.${password_field_type} i.show-hide-password`).removeClass("fa-eye-slash");
+            $(`.${password_field_type} i.show-hide-password`).addClass("fa-eye");
+        }
+    });
+}
