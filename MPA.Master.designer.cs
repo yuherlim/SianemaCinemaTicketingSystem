c1cb47f22fb19d912ee9966f7919b8dcea41590a
Johnson Ong <ong5828@gmail.com>
2024-04-02 19:18:36 +0800

    MPA

--- MPA.Master.designer.cs
+++ MPA.Master.designer.cs
@@ -31,5 +31,14 @@ namespace SianemaCinemaTicketingSystem
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// Js control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Js;
     }
 }
